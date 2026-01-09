10550656d87a3054e0d4677d7d3787c547255ca1
emmlkkn <mikael.saleteg@ericsson.com>
2026-01-09 11:24:04 +0100

    Add ruff to lsp and format on save

--- init.lua
+++ init.lua
@@ -684,6 +684,8 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        ruff = {},
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -769,7 +771,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff_format' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
